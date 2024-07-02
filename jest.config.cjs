078b2d3573371338f3c89a92b02187072aaf95ba
Develing1991 <completed0728@gmail.com>
2024-07-02 15:12:04 +0900

    Add syteMock.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,4 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jest-environment-jsdom',
     modulePathIgnorePatterns: ['./Temp.spec.tsx'], //, '__test__'],
+    moduleNameMapper: {
+        '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.cjs',
+    },
 }
