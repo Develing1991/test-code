b8525bfa3d6974bd92f694c32d00d37759674f33
Develing1991 <completed0728@gmail.com>
2024-07-03 10:20:01 +0900

    feat: add cypress procjectId

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:5173",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    // projectId: process.env?.REACT_APP_PROJECT_ID,
+    projectId: 'c6kut6',
+    e2e: {
+        baseUrl: 'http://localhost:5173',
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
-});
+})
