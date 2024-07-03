3767976b11cd954bcb331410b08fbf308dcaabe3
Develing1991 <completed0728@gmail.com>
2024-07-03 10:20:05 +0900

    feat: add cypress procjectId

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-    // projectId: process.env?.REACT_APP_PROJECT_ID,
     projectId: 'c6kut6',
     e2e: {
         baseUrl: 'http://localhost:5173',
